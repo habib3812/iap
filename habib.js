07ad7958d66a9ee94c79abd688ce1660137fc6df
Habibur Rahman Raju <86669697+habib3812@users.noreply.github.com>
2022-09-05 17:40:12 +0600

    Add files via upload

---
+++ habib.js
@@ -0,0 +1,11 @@
+function calculate(a, b){
+    return a + b;
+}
+
+function addition(a, b){
+    return a + b;
+}
+
+fu
+
+console.log(calculate(3, 5))
---
+++ scoop.js
@@ -0,0 +1,11 @@
+// const name = "habib"
+
+function khamakha(){
+    const name = "Mariyam"
+    if (true){
+        const name = "me
+    }
+}
+console.log(name)
+
+// khamakha()
